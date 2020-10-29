4b826b4ab5e5eaabdf6a58be802ec6b2fc382601
Will Kelly <willkelly9617@gmail.com>
2020-10-29 10:24:03 -0500

    refactored some more

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ let negativeSign = true;
 let percentageSign = true;
 let negLastNum = new RegExp(/-{1}\d*(\.?)\d+$/); //breaks subtracting functionality including last # in regex
 // let digits = new RegExp(/\d*(\.?)\d+$/); //need to retweak flipping nums
-let digits = new RegExp(/-*\d*(\.?)\d+$/); //original digits
+let digits = new RegExp(/-*\d*(\.?)\d*$/); //matches possible numbers with neg and decimals
 let operatorsReg = new RegExp(/[+*/-]/gi);
-// console.log(buttons);
+let endsWithOperator = new RegExp(/[+*/-]\s*$/); //white space at end due to keeping spacing in mathexpression
 
 function clear() {
   display.textContent = '';
@@ -37,59 +37,41 @@ function deleteOne() {
   if (lastBtn && lastBtn.classList.contains('equal-btn')) {
     return clear();
   }
+
   //For operators or numbers, delete last and update text and expression
+  //todo: there is some wonkiness in here with the spacing for operators.  REally need to split this logic into slicing out numbers vs. slicing operators out.
   display.textContent = display.textContent.slice(
     0,
     display.textContent.length - 1
   );
-  mathExpression = mathExpression.slice(0, mathExpression.length - 1); //? not sure why white space works for operands but not textcontent
+  mathExpression = mathExpression.slice(0, mathExpression.length - 1);
 
   //updating last number based on whether operaand is at end
   if (lastBtn && lastBtn.classList.contains('number')) {
-    if (display.textContent.match(/[+*/-]$/i)) {
+    if (display.textContent.match(endsWithOperator)) {
       lastNumber = display.textContent.match(/\d*(\.?)\d+/)[0];
     } else lastNumber = display.textContent.match(digits)[0];
   }
   lastBtn = this;
 }
 
-//@#===============  manage Numbers  =============
+//@#===============  MANAGE NUMBERS  =============
 function manageNumber() {
   let value = this.innerText;
   let dataValue = this.dataset.value;
 
-  console.log(value);
+  //clearning placeholder
   if (!lastBtn) {
     display.innerText = '';
-    // displayExpression.innerText = '';
   }
+
+  //clear if last button was equal (i.e start over)
   if (lastBtn && lastBtn.classList.contains('equal-btn')) {
     clear();
   }
-  // if (lastBtn && lastBtn.classList.contains('number')) {
-  if (display.textContent.match(digits)) {
-    //Better method of determining last number
-    // lastNumber += value;
-
-    //!must add input since match returns an object
-    display.textContent += value;
-    // displayExpression.innerText += value;
-    mathExpression += dataValue;
-  } else {
-    // lastNumber = value;
-    display.textContent += value;
-    // displayExpression.innerText = value;
-    mathExpression += dataValue;
-  }
-
-  lastNumber = display.textContent.match(digits)[0];
-  //check for previous negative button for last number
-  // if (lastBtn && lastBtn.classList.contains('flip-sign')) {
-  //   lastNumber = `-${lastNumber}`;
-  /* else if (!negativeSign) {
-      lastNumber = lastNumber.replace(`- ${lastNumber}`, value);
-    } */
-  // }
+  display.textContent += value;
+  mathExpression += dataValue;
+  lastNumber = display.textContent.match(digits)[0]; //matching to end of last numbers
 
   lastBtn && lastBtn.classList.contains('flip-sign')
     ? negativeSign == false
@@ -98,41 +80,40 @@ function manageNumber() {
   checkfortooLong();
 }
 
-//@# ===============  manage Operator  =============
-function manageOperator(event) {
-  console.log(this.dataset.value);
+//@# ===============  MANAGE Operator  =============
+
+function manageOperator() {
+  //can't push and operator button first
   if (!lastBtn || display.textContent.length == 0) {
     return;
   }
-  //todo: Flawed. Need to fix. 6 + 99 del + x renders an error.  Need to eval action based upon last char in textcontent (see regex matching above in delete fxn for help how)
-  if (
-    (lastBtn && lastBtn.classList.contains('operator')) ||
-    (lastBtn &&
-      lastBtn.classList.contains('delete') &&
-      !display.textContent.match(digits))
-  ) {
+
+  if (lastBtn.classList.contains('flip-sign') && !mathExpression.match(/\d$/)) {
+    return; //don't want to do anything if negative was previous.
+  }
+  //replace previous operator
+  if (mathExpression.match(endsWithOperator)) {
     mathExpression = mathExpression.substring(0, mathExpression.length - 2);
     display.textContent = display.textContent.substring(
       0,
       display.textContent.length - 2
     );
   }
+
+  //@% Main 3 things to do when operan is pushed everytime
   mathExpression += ' ' + this.dataset.value + ' '; //spaces around operands
   display.textContent += ` ${this.textContent} `; //? why won't this put the spaces?
   currentOperator = this.dataset.value;
-
-  // // lastBtn && lastBtn.classList.contains('flip-sign')
-  //   ? negativeSign == true
-  //   : negativeSign == false;
 
   negativeSign = true;
   lastBtn = this;
   checkfortooLong();
 }
 
-//@# ===============  manage percents  =============
+//@# ===============  MANAGE PERCENTS  =============
 function percent() {
-  console.log(this);
+  //todo: this is a bit broken.  If you already have a decimal or are taking percents beyond .00 place, then it doesn't work right.  The replace function is limited to those places.
+
   if (lastBtn && lastBtn.classList.contains('number')) {
     let percentNumber = String(lastNumber * 0.01);
     display.textContent = display.textContent.replace(
@@ -143,10 +124,12 @@ function percent() {
     lastNumber = percentNumber;
     percentageSign = false;
   }
+
   if (lastBtn && lastBtn.classList.contains('equal-btn')) {
     display.textContent = parse(display.textContent * 0.01);
     mathExpression = parse(mathExpression) * 0.01;
   }
+
   if (lastBtn == this) {
     if (percentageSign) {
       let percentNumber = String(lastNumber * 0.01);
@@ -173,41 +156,43 @@ function percent() {
   lastBtn = this;
 }
 
-//@# ===============  manage equals button  =============
+//@# ===============  MANAGE EQUALS BUTTON  =============
 //@! clicking the equals button
 function evaluate(event) {
   if (lastBtn == this) {
     mathExpression = `${parse(
       mathExpression
-    )} ${currentOperator} ${lastNumber}`;
+    )} ${currentOperator} ${lastNumber}`; //the spaces in these template literals ensure that doing an operation such as 7 - -3 doesn't become 7--3 and break the parser
   }
-  display.textContent = parse(mathExpression);
-  negativeSign = true;
 
-  //@ Can just truncate display to keep everything beneath a certain text size.
-  checkfortooLong();
+  //calculate result
+  display.textContent = parse(mathExpression);
+
   //Rounds decimals
   if (display.textContent.length >= 11 && display.textContent.includes('.')) {
     display.textContent = parse(mathExpression).toFixed(3);
   }
   negativeSign = true;
   lastBtn = this;
+  checkfortooLong();
 }
 
 //?How does this work?  Used due to mdn saying never use eval
 //?Got it  from this website:  Check here to remind yourself: https://dev.to/spukas/everything-wrong-with-javascript-eval-35on
+
+//@# -------------- MANAGE PARSING -------------- */
 function parse(str) {
   return Function(`return (${str})`)();
 }
 
 //@# ===============  Check for too long  =============
 function checkfortooLong() {
-  if (display.textContent.length > 50) {
+  if (display.textContent.length > 25) {
     display.textContent = "Too many #'s for my little width";
   }
 }
 
-//@? I used this version when I wasn't defining last num with neg in it.  The problem is the regex is matching the minus sign.
+//@# this is called within flip sign whenever the mathexpression would match numbers at the end to invert them
 function invertNumbers() {
   if (display.textContent.match(negLastNum)) {
     let index = display.textContent.lastIndexOf(lastNumber);
@@ -223,47 +208,11 @@ function invertNumbers() {
     display.textContent = display.textContent
       .substring(0, index)
       .concat(newNum);
-    // mathExpression = mathExpression.replace(lastNumber, newNum);
     mathExpression = mathExpression.substring(0, index).concat(newNum);
     lastNumber = newNum;
     negativeSign = !negativeSign;
   }
 }
-
-//@? Used this version when I was defiing last number w/ the negative in it.
-/* function invertNumbers() {
-  console.log('flipping num');
-  if (negativeSign) {
-    let index = display.textContent.lastIndexOf(lastNumber);
-    let newNum = lastNumber.replace(lastNumber, `-${lastNumber}`);
-    console.log({ newNum });
-    // display.textContent = display.textContent.replace(lastNumber, newNum);
-    display.textContent = display.textContent
-      .substring(0, index)
-      .concat(newNum);
-    // mathExpression = mathExpression.replace(lastNumber, newNum);
-    mathExpression = mathExpression.substring(0, index).concat(newNum);
-    lastNumber = newNum;
-    negativeSign = !negativeSign;
-  } else if (!negativeSign) {
-    let index = display.textContent.lastIndexOf(lastNumber);
-    // display.textContent.substring(0, index).concat(xnum)
-    let positiveLastNum = String(Math.abs(lastNumber.match(digits)[0]));
-    console.log(positiveLastNum);
-    newNum = lastNumber.replace(`${lastNumber}`, positiveLastNum);
-    // display.textContent = display.textContent.replace(
-    //   lastNumber,
-    //   positiveLastNum
-    // );
-    display.textContent = display.textContent
-      .substring(0, index)
-      .concat(positiveLastNum);
-    // mathExpression = mathExpression.replace(lastNumber, positiveLastNum);
-    mathExpression = mathExpression.substring(0, index).concat(positiveLastNum);
-    lastNumber = positiveLastNum;
-    negativeSign = !negativeSign;
-  }
-} */
 
 //@# ===============  inverse sign  =============
 function flipSign() {
@@ -291,52 +240,21 @@ function flipSign() {
 
   //@% handling this (+/-) as last inverse button
   if (lastBtn == this) {
+    //this conditional handles things like  9 + -... then flipping that negative on and off
+    if (mathExpression.match(endsWithOperator)) {
+      let endIndex = mathExpression.length - 1;
+      if (mathExpression.charAt(endIndex) == '-') {
+        mathExpression = mathExpression.substring(0, endIndex);
+        display.textContent = display.textContent.substring(0, endIndex);
+        return;
+      } else {
+        mathExpression += '-';
+        display.textContent += '-';
+        return;
+      }
+    }
     invertNumbers();
   }
-
-  /* 
-    
-    console.log('last btn this');
-    // negativeSign = false;
-    if (display.textContent.match(digits)) {
-      if (negativeSign) {
-        let index = display.textContent.lastIndexOf(lastNumber);
-        console.log({ negativeSign });
-        newNum = lastNumber.replace(lastNumber, `-${lastNumber}`);
-        console.log({ newNum });
-        // display.textContent = display.textContent.replace(lastNumber, newNum);
-        display.textContent = display.textContent
-          .substring(0, index)
-          .concat(newNum);
-        // mathExpression = mathExpression.replace(lastNumber, newNum);
-        mathExpression = mathExpression.substring(0, index).concat(newNum);
-        lastNumber = newNum;
-        negativeSign = !negativeSign;
-        return;
-      } else if (!negativeSign) {
-        let index = display.textContent.lastIndexOf(lastNumber);
-        newNum = lastNumber.slice(1);
-        console.log({ newNum });
-        // display.textContent = display.textContent.replace(lastNumber, newNum);
-        display.textContent = display.textContent
-          .substring(0, index)
-          .concat(newNum);
-        // mathExpression = mathExpression.replace(lastNumber, newNum);
-        mathExpression = mathExpression.substring(0, index).concat(newNum);
-        lastNumber = lastNumber.slice(1);
-        negativeSign = !negativeSign;
-        return;
-      } */
-
-  /*   if (negativeSign) {
-      display.textContent += '-';
-      mathExpression += '-';
-      negativeSign = !negativeSign;
-    } else if (!negativeSign) {
-      display.textContent = display.textContent.replace('-', '');
-      mathExpresion = mathExpression.replace('-', '');
-      negativeSign = !negativeSign;
-    } */
 
   //@% handling equal inverse
   if (lastBtn && lastBtn.classList.contains('equal-btn')) {
@@ -376,14 +294,8 @@ numbers.forEach((button) => button.addEventListener('click', manageNumber));
 operators.forEach((operator) =>
   operator.addEventListener('click', manageOperator)
 );
-
 clearBtn.addEventListener('click', clear);
 deleteBtn.addEventListener('click', deleteOne);
 equalBtn.addEventListener('click', evaluate);
 percentBtn.addEventListener('click', percent);
 negativeBtn.addEventListener('click', flipSign);
-
-//Still needs work on + - sign to refine.  Things like +/- 7 +  +/- 7.  Break the calc.  It also breaks in  places regarding going ham on the plus minus button messing up the expression due to the way the last number is rendered inside the mange number function.
-
-//Also need some logic on plus minus button if the last button was the equals button.
-//That button is so hard to manage.  Although I wonder if it would be easier to push the values into an mathexpression array?
